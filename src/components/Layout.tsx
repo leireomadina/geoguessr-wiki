c4c88855461bf0dfa6c7e11904d034b8fc3611c5
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-05-17 14:50:32 +0200

    Add new layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import "../styles/Layout.css";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="layout">
+      <header className="header">
+        <nav className="nav-container">
+          <Link to="/" className="logo">
+            🌍 GeoGuessr Wiki
+          </Link>
+          <div className="nav-links">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </div>
+        </nav>
+      </header>
+
+      <main className="content">
+        <Outlet />
+      </main>
+
+      <footer className="footer">
+        <div className="footer-content">
+          <p>GeoGuessr Wiki &copy; {new Date().getFullYear()}</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
