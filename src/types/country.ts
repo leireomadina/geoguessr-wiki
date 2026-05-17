0a5c3701b0cfd1950b2f5efcc25330071a303175
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-05-17 18:01:30 +0200

    Add continent as an enum

--- src/types/country.ts
+++ src/types/country.ts
@@ -1,11 +1,17 @@
 export type Difficulty = "easy" | "medium" | "hard" | "very_hard";
-
 export type DrivingSide = "left" | "right";
+export type Continent =
+  | "Africa"
+  | "Asia"
+  | "Europe"
+  | "North America"
+  | "Oceania"
+  | "South America";
 
 export interface Country {
   id: string; // ISO Code (e.g., 'ES' for Spain)
   name: string;
-  continent: string;
+  continent: Continent;
   studyLinks: string[];
   difficulty: Difficulty;
   drivingSide: DrivingSide;
