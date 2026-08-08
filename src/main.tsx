01b192fdf7745981f174bd991225b6f41873c1c6
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-08-08 17:37:53 +0200

    Fix deep-link restore to include base path

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const basename = '/geoguessr-wiki'
+
 const redirect = sessionStorage.getItem('gh-pages-redirect')
 if (redirect) {
   sessionStorage.removeItem('gh-pages-redirect')
-  window.history.replaceState({}, '', redirect)
+  window.history.replaceState({}, '', `${basename}${redirect}`)
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/geoguessr-wiki">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
